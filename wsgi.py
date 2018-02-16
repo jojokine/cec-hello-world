497410be8448dc1b35f685396718a94f09e7a31a
jojokine <oskari.jokinen@helsinki.fi>
2018-02-16 10:21:46 +0200

    add timestamping and hostname logfile write

--- wsgi.py
+++ wsgi.py
@@ -6,8 +6,10 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
     
+    file = open(“/mnt/hello-world-storage/logfile”,”w”)
+    file.write("access from: "+socket.gethostname()+" at: "+time.ctime()+"\n") 
+    
     return "Hello World! Greetings from "+socket.gethostname()+"\n"
-
 
 if __name__ == "__main__":
     application.run()
