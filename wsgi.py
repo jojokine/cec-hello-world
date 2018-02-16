e09a4d8c8d50b35b7e59af5f79bb56670bd871f7
jojokine <oskari.jokinen@helsinki.fi>
2018-02-16 16:53:45 +0200

    configure file to be readable and writable

--- wsgi.py
+++ wsgi.py
@@ -7,7 +7,7 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
     
-    file = open('/mnt/hello-world-storage/logfile','w')
+    file = open('/mnt/hello-world-storage/logfile','r+')
     file.write("access from: "+socket.gethostname()+" at: "+time.ctime()+"\n")
     
     return "Hello World! Greetings from "+socket.gethostname()+"\n"+file.readlines()+"\n"
